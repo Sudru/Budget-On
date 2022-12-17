bfa5b35ba911c41c03cce8c05a6818c89bbbc4af
Sudarshan <sudarshandevkota22@gmail.com>
2022-12-17 21:21:37 +0545

    started

---
+++ src/main/java/com/sudru/BudgetOn/BudgetOnApplication.java
@@ -0,0 +1,13 @@
+package com.sudru.BudgetOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetOnApplication.class, args);
+	}
+
+}
