3166c23012c87753d3f316d3604b4c9068f07da6
Sudarshan <sudarshandevkota22@gmail.com>
2022-12-20 18:43:02 +0545

    model mapper bean created

--- src/main/java/com/sudru/BudgetOn/BudgetOnApplication.java
+++ src/main/java/com/sudru/BudgetOn/BudgetOnApplication.java
@@ -1,7 +1,9 @@
 package com.sudru.BudgetOn;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BudgetOnApplication {
@@ -10,4 +12,8 @@ public class BudgetOnApplication {
 		SpringApplication.run(BudgetOnApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
