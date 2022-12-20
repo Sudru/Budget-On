1e66e76df51316f41a3beca8671eec94aae5019f
Sudarshan <sudarshandevkota22@gmail.com>
2022-12-20 18:45:36 +0545

    user repository method to getUserByEmail, user service implementation for register user and user controller

--- src/main/java/com/sudru/BudgetOn/repository/UserRepository.java
+++ src/main/java/com/sudru/BudgetOn/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.sudru.BudgetOn.repository;
 import com.sudru.BudgetOn.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,String> {
 
+    Optional<User> getByUsername(String s);
 }
--- src/main/java/com/sudru/BudgetOn/service/UserService.java
+++ src/main/java/com/sudru/BudgetOn/service/UserService.java
@@ -2,17 +2,30 @@ package com.sudru.BudgetOn.service;
 
 
 import com.sudru.BudgetOn.dto.RegisterDto;
+import com.sudru.BudgetOn.entity.BudgetAmount;
 import com.sudru.BudgetOn.entity.User;
 import com.sudru.BudgetOn.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
     private  final UserRepository userRepository;
     public void registerUser(RegisterDto registerDto) {
-
+        User user = userRepository.getByUsername(registerDto.getUsername()).orElse(null);
+        if (user != null) {
+            throw new RuntimeException("User already exists.");
+        }
+        user = new User();
+        user = modelMapper.map(registerDto,User.class);
+        user.setBudget(new BudgetAmount());
+        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
+        userRepository.save(user);
     }
 
 }
