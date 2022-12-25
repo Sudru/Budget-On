5aa299d5a0eaf2a6d5f408ad81a1dae6d0187076
Sudarshan <sudarshandevkota22@gmail.com>
2022-12-25 12:25:52 +0545

    transaction entity added

---
+++ src/main/java/com/sudru/BudgetOn/entity/Transaction.java
@@ -0,0 +1,26 @@
+package com.sudru.BudgetOn.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+@Entity(name = "transaction")
+@Data
+public class Transaction {
+    @Id
+    private int id;
+    @CreationTimestamp
+    private Date timestamp;
+
+    private double amount;
+    private boolean isPending;
+    private TransactionType transactionType;
+    private String note;
+    private String senderOrReceiver;
+    @ManyToOne
+    private User user;
+}
---
+++ src/main/java/com/sudru/BudgetOn/entity/TransactionType.java
@@ -0,0 +1,5 @@
+package com.sudru.BudgetOn.entity;
+
+public enum TransactionType {
+    INCOME,EXPENDITURE
+}
